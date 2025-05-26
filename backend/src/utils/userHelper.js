15d9db832adf4173001f7ffa60a83f92fea5bd09
Diogo Regadas <Diogo.Regadas@outlook.pt>
2025-05-26 23:15:38 +0100

    reset bcrypt to bcryptjs

--- backend/src/utils/userHelper.js
+++ backend/src/utils/userHelper.js
@@ -1,5 +1,5 @@
 const User = require('../models/mongoose/userMongo'); // Mongoose schema
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const createUser = async (userModel) => {
