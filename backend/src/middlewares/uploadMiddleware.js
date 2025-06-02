1330b01d3261f2a40d021e54acb2a26f15ee1737
Diogo Regadas <Diogo.Regadas@outlook.pt>
2025-06-02 17:17:05 +0100

    ..

--- backend/src/middlewares/uploadMiddleware.js
+++ backend/src/middlewares/uploadMiddleware.js
@@ -1,18 +1,20 @@
 const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
-const path = require('path');
 
 const storage = new GridFsStorage({
   url: process.env.MONGO_URI,
-  file: async (req, file) => {
-    return {
-      filename: `${Date.now()}-${file.originalname}`,
-      bucketName: 'anexos',
-      metadata: {
-        originalname: file.originalname,
-        mimetype: file.mimetype
-      }
-    };
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      const fileInfo = {
+        filename: `${Date.now()}-${file.originalname}`,
+        bucketName: 'anexos',
+        metadata: {
+          originalname: file.originalname,
+          mimetype: file.mimetype
+        }
+      };
+      resolve(fileInfo);
+    });
   }
 });
 
