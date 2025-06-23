c681cd5a6ee46b67b4d4b9dfd668e5c014a429ab
Diogo Regadas <Diogo.Regadas@outlook.pt>
2025-06-23 12:39:01 +0100

    .

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -17,7 +17,7 @@ exports.loginUser = async (req, res) => {
     try {
         const resultado = await UserService.loginUser(req.body);
         await LogService.criarLog({
-          userId: utilizador._id,
+          userId: resutlado.utilizador._id,
           tipo: 'auth',
           detalhe: 'Login efetuado com sucesso'
         });
