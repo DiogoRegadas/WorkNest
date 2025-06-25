09a9089ccc5d5304315dcf5086518d24aae28ac4
Diogo Regadas <Diogo.Regadas@outlook.pt>
2025-06-25 14:59:21 +0100

    ..

--- backend/src/models/mongoose/Log.js
+++ backend/src/models/mongoose/Log.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const LogSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Utilizador',
+    ref: 'User',
     required: false,
   },
   projetoId: {
