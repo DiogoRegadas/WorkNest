76bfc19473a99e4523070bc49f3056c64f2a27f5
Diogo Regadas <107991210+DiogoRegadas@users.noreply.github.com>
2025-11-19 15:09:41 +0000

    Update firebaseConfig.js

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -3,15 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyApiTOzAFOF4z7m1mz10hdFv00c2zr0Lvo",
-  authDomain: "worknest-b0d90.firebaseapp.com",
-  projectId: "worknest-b0d90",
-  storageBucket: "worknest-b0d90.firebasestorage.app",
-  messagingSenderId: "469291518448",
-  appId: "1:469291518448:web:e3929419000e1b3562b535",
-  measurementId: "G-53D9E30HXS"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app, "worknest");
