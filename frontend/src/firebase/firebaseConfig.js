08fd2f5be4f1a9a9e0c4254ca867c98531f53c05
Diogo Regadas <107991210+DiogoRegadas@users.noreply.github.com>
2025-11-19 15:11:20 +0000

    Update firebaseConfig.js

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -3,7 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app, "worknest");
